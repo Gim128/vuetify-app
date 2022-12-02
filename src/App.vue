35a0c5cc8687d9f7fc42a47924a5d2760dcbdfc5
ican-graphics-01 <lankaican@gmail.com>
2022-12-02 15:39:54 +0530

    init vue project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+
+      style="padding:0px 90px"
+
+      app
+      color="white"
+      light
+      elevation="6"
+      elevate-on-scroll
+    >
+
+    <v-toolbar-title>
+      Vuetify
+    </v-toolbar-title>
+
+    <v-spacer />
+
+    <v-list>
+      <v-list-item>
+        <v-list-item-title> Home </v-list-item-title>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title> About </v-list-item-title>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title> Home </v-list-item-title>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title> Home </v-list-item-title>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title> Home </v-list-item-title>
+      </v-list-item>
+
+    </v-list>
+      
+    </v-app-bar>
+      Vuetify
+    <v-main>
+      
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data(){
+    return {
+      menus:[
+        {title:'Home', route:'home'},
+        {title:'About us', route:'about-us'},
+        {title:'Portfolio', route:'portfolio'},
+        {title:'Project', route:'project'},
+        {title:'Gallery', route:'gallery'},
+        {title:'Contact', route:'contact'}
+      ]
+    }
+  }
+
+};
+</script>
